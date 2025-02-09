b1f171e93994ca48fd54674750a56047fbedd7bd
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-02-09 21:03:05 +0530

    Get All user Events

--- controller/event.js
+++ controller/event.js
@@ -7,7 +7,6 @@ const createEvent = async (req, res, next) => {
     const user = await sql("SELECT * FROM users WHERE id =$1", [userID]);
     if (user.length) {
       const { title, description, eventDate, eventTime } = req.body;
-      console.log(`${title} ${description} ${eventDate} ${eventTime}`);
       const newEvent = await sql(
         "INSERT INTO events (user_id, title, description, event_date, event_time) VALUES ($1, $2, $3, $4, $5) RETURNING *, event_date::TEXT AS event_date",
         [userID, title, description, eventDate, eventTime]
@@ -23,7 +22,23 @@ const createEvent = async (req, res, next) => {
   }
   return next(new UnauthorizedError("Unauthorized!"));
 };
-const getAllUserEvents = (req, res) => {};
+const getAllUserEvents = async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    const { userID } = req.params;
+    const user = await sql("SELECT * FROM users WHERE id =$1", [userID]);
+    if (user.length) {
+      const allEvents = await sql(
+        "SELECT *, event_date::TEXT AS event_date FROM events WHERE user_id = $1",
+        [userID]
+      );
+
+      return res.json({ events: allEvents });
+    } else {
+      return res.json({ msg: `Can't find the user with id ${userID}` });
+    }
+  }
+  return next(new UnauthorizedError("Unauthorized!"));
+};
 const getEvent = (req, res) => {};
 const deleteEvent = (req, res) => {};
 const updateEvent = (req, res) => {};
