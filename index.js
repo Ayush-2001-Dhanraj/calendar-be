00f4d6dd09bec4586ea245f157cf73096418f602
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-02-10 22:59:58 +0530

    Fix Cors and cookie errors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import cors from "cors";
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
