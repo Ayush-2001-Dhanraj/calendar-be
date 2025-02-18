635a91c9b0f29a3bedcf51f22047320c37e097fe
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-02-18 22:17:27 +0530

    rem secure cookie thing

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
-      secure: process.env.NODE_ENV === "production", // Secure in production
+      secure: false, // Secure in production
       httpOnly: true,
       sameSite: "lax",
     },
