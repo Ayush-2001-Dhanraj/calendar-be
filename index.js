4ee514a4d1d8f592adab219ed1a7f5699835efc2
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-02-18 22:45:56 +0530

    auto

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
-      secure: false, // ✅ Secure only in production
+      secure: "auto",
       httpOnly: true,
       sameSite: "none",
     },
